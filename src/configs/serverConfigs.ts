dac1f794d5fa541175071b2872aeb6f5fe503fc3
KirillBuiko <k.buiko04@mail.ru>
2023-12-15 03:56:30 +0700

    Change servers ports

--- src/configs/serverConfigs.ts
+++ src/configs/serverConfigs.ts
@@ -1,6 +1,6 @@
 import type {ServerConfigsType} from "@/app/types/ServerConfigType";
 
 export const serverConfigs: ServerConfigsType = {
-    grpcServer: {port: 5051},
-    restApiServer: {port: 5052}
+    restApiServer: {port: 5051},
+    grpcServer: {port: 5052}
 }
