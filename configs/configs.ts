37ab5b581ebfd90f2aba4fe5db0cc154a521a574
KirillBuiko <k.buiko04@mail.ru>
2024-05-22 23:57:27 +0700

    Add bash start scripts, write pid on start

--- configs/configs.ts
+++ configs/configs.ts
@@ -1,5 +1,6 @@
 export const configs = {
     ENDPOINT_CONNECTION_DEADLINE: 1000,
     UI_HOST: "http://localhost:5000",
+    PID_PATH: "./core.pid",
     PROTO_PATH: "./node_modules/luna-proto-files/requests.proto"
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -3,18 +3,25 @@ import {serverConfigs} from "../../configs/serverConfigs";
 import {EndpointsManager} from "@/endpoints/EndpointsManager";
 import {RequestManager} from "@/request-manager/RequestManager";
 import {endpointConfigs} from "../../configs/endpointConfigs";
+import fs from "fs";
+import {configs} from "../../configs/configs";
 
 const serversManager = new ServersManager();
 const endpointsManager = new EndpointsManager();
 const requestsManager = new RequestManager({endpointsManager});
 
+function writePid() {
+    const pid = process.pid;
+    fs.createWriteStream(configs.PID_PATH).end(pid.toString());
+}
 async function main() {
     try {
+        writePid();
         await endpointsManager.initAll(endpointConfigs);
         await serversManager.startAll(serverConfigs, requestsManager);
     }
     catch (e) {
-        console.log("Core start failed");
+        console.log("Core start failed: ", e);
     }
 }
 
